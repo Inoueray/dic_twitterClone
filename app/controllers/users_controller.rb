bfcfed2532b96ada2686c170e84bf635f1ae59be
Rei Inoue <rei.inoue01@gmail.com>
2018-04-22 22:18:46 +0900

    revise to log in seamlessly after sign up and some minor change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:notice] = "ユーザー登録完了！"
       redirect_to user_path(@user.id)
     else
       render 'new'
